657f9fc667c13e5db024c3d806c49a97463887bd
rahulgundawar98 <rahulgundawar98@gmail.com>
2023-02-14 20:30:43 +0530

    Calculate Percentage of flip coin in Heads and Tails UC1_FlipCoinPercentage

---
+++ BasicCorePrograms/FlipCoinPercentage.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    public class FlipCoinPercentage
+    {
+        public static void Check_Percentage()
+        {
+            int HeadCount = 0 , TailCount=0;
+            Console.WriteLine("Enter the value to flip the coin");
+            int count = Convert.ToInt32(Console.ReadLine());
+            Random random = new Random();
+            for(int i=0;i<count; i++)
+            {
+                double input =random.NextDouble();
+                if(input<0.5)
+                {
+                    HeadCount++;
+                }
+                else
+                {
+                    TailCount++;
+                }
+            }
+            Console.WriteLine("HeadCount:{0}  and TailCount:{1}",HeadCount,TailCount);
+            double HeadPercentage = (HeadCount * 100) / 10;
+            double TailPercentage = (TailCount * 100) / 10;
+            Console.WriteLine("HeadPercentage is {0}%",HeadPercentage);
+            Console.WriteLine("TailPercentage is {0}%" ,TailPercentage);
+        }
+    }
+}
--- BasicCorePrograms/Program.cs
+++ BasicCorePrograms/Program.cs
@@ -7,6 +7,15 @@ namespace BasicCorePrograms
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Basic Core Programs");
+            Console.WriteLine("Choose option from below");
+            Console.WriteLine("1.Flip Coin Percentage");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch(option)
+            {
+                case 1:
+                    FlipCoinPercentage.Check_Percentage();
+                    break;
+            }
         }
     }
 }
