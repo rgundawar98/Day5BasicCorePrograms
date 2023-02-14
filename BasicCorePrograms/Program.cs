5c8a1d63e55f5139dc50f28303f042843c0855b1
rahulgundawar98 <rahulgundawar98@gmail.com>
2023-02-14 20:17:52 +0530

    Adding welcome message

---
+++ BasicCorePrograms/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BasicCorePrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Basic Core Programs");
+        }
+    }
+}
